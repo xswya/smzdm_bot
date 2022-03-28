8fce21f04edcb364f68eaf2ae8782b6795edea83
xswya <sproink@vip.qq.com>
2022-03-28 14:56:34 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=LBo3wShwIl1PIqO2eJhlnr5; __jsluid_s=fc53c828f646a22261bbc11b3a26b3cc; device_id=1026835063161974813658862184d8f5a88790b8a8564f296ccef16424; __jsluid_h=45aafe973e14f687fac36df60b2863be; r_sort_type=score; __gpi=00000000-0000-0000-0000-000000000000&c216ZG0uY29t&Lw==; homepage_sug=e; userId=user:3782784892|3782784892; smzdm_user_source=1C0BF2F0ABFAA0F45988F6C848A1F0F6; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1635815833,1635994166; shequ_pc_sug=b; _ga_09SRZM2FDD=GS1.1.1636074882.7.1.1636076498.0; _zdmA.vid=*; footer_floating_layer=0; ad_date=28; ad_json_feed={}; sensorsdata2015jssdkcross={"distinct_id":"3782784892","first_id":"17920832467275-092cbc94eb0a62-5230164c-1810560-17920832468da0","props":{"$latest_traffic_source_type":"直接流量","$latest_search_keyword":"未取到值_直接打开","$latest_referrer":""},"$device_id":"17920832467275-092cbc94eb0a62-5230164c-1810560-17920832468da0"}; sess=AT-miaXg+TzTgjXueAjPBhkAOXMmJnxH06yV+IOTFJO0d9DUfzHhSIlyLgJHegfQ1jwMXn9pV+tPKz33hTy9y1+LpvlSY+ftUoUsT+WpdPm+r2k2+qqU3ApmqkL; user=user:3782784892|3782784892; smzdm_id=3782784892; _zdmA.uid=ZDMA.47nIqEDsf.1648450344.2419200; bannerCounter=[{"number":1,"surplus":1},{"number":0,"surplus":1},{"number":1,"surplus":1},{"number":0,"surplus":1},{"number":1,"surplus":1},{"number":0,"surplus":1}]; _zdmA.time=1648450372785.0.https://www.smzdm.com/'
 
 
 """
